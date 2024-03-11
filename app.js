9531cde9af47e55e0a3591f07342dff2f30bb824
Reem Abdulglil Hassan Saleh <reemabdulglilhassansaleh@Reems-MacBook-Air.local>
2024-03-11 22:53:54 +0300

    first commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const request = require("request")
 const url = "https://api.weatherapi.com/v1/current.json?key=7f97e74ef23b418c97a155211230503&q=yamen"
 
 request ({url , json : true  } , (error , response) => {
-  // console.log(response.body)
+  console.log(response.body)
     if (error) {
         console.log("ERROR HAS OCCURED")
     } else if (response.body.error){
